1d41f9d2ab190932d0464291cc35393ebf0458d6
hugolhld <hugo.lhernould@gmail.com>
2025-02-10 10:51:54 +0100

    feat: add init server and first route

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		log.Printf("%s %s %s", r.Method, r.RequestURI, time.Since(start))
+	})
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "OK")
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", healthCheckHandler)
+
+	loggedMux := loggingMiddleware(mux)
+
+	fmt.Println("Serveur démarré sur :8080")
+	log.Fatal(http.ListenAndServe(":8080", loggedMux))
+}
